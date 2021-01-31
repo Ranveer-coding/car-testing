a0ca99905c4c4e125e751e9eb1d42fc0e5f51a38
Ranveer-coding <75207357+Ranveer-coding@users.noreply.github.com>
2021-01-31 11:03:00 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var car,wall;
+var speed,weight;
+var car;
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(55,90)
+  weight=random(400,1500)
+  car=createSprite(50,200,50,50)
+  wall=createSprite(1500,200,60,60,height/2)
+  wall.shapeColor=("white")
+  car.velocity=speed;
+  car.shapeColor=("white")
+}
+
+
+
+
+if(wall.x < (car.width+wall.width)/2){
+
+  car.velocityX=0;
+  var deformation=0.5
+  
+    }
+
+
+function draw() {
+  background("black"); 
+
+  if(wall.x < (car.width+wall.width)/2){
+
+    car.velocityX=0;
+    var deformation=0.5 weight,speed,speed/22509;}
+    if (deformation>180{
+      
+
+        car.shapeColor=color(230,230,0)
+      }
+      if(deformation<100)
+      {
+        car.shapeColor=color(0,255,0)
+      }
+    
+      
+
+
+
+
+  drawSprites();
+}
